34dfb84c4912d5676c7de92443cfdcde7ecbb78f
Samarit <kislyakaleksey@gmail.com>
2024-06-23 19:50:06 +0300

    login controller @body

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,18 +1,31 @@
-import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { Response } from 'express';
-import { AuthGuard } from 'src/guards/auth/auth.guard';
 import { AuthService } from 'src/services/auth/auth.service';
+
+interface LoginDTO {
+  login: string;
+  password: string;
+}
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Req() req: any, @Res({ passthrough: true }) res: Response) {
-    const { login, password } = req.body;
-
-    console.log(req.body);
-    console.log({ login, password });
+  async login(
+    @Req() req: any,
+    @Body() body: LoginDTO,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { login, password } = body;
 
     if (!login || !password)
       return { error: 'Login and password are required' };
@@ -20,9 +33,7 @@ export class AuthController {
     const result = await this.authService.signIn(login, password);
 
     if (result && result.token) {
-      console.log({ result });
       res.cookie('token', result.token, { httpOnly: true, secure: false });
-
       return result;
     }
   }
